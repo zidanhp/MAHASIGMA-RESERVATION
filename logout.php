6f17c416dcb54da9ba513302ed34ed3d199033b0
Zidan Hamana Putra <157278601+zidanhp@users.noreply.github.com>
2025-01-08 15:26:53 +0700

    logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,20 @@
 <?php
+/*
+Pseudocode:
+
+1. Mulai session pengguna
+    - Mulai session untuk mengakses data session yang sudah ada.
+
+2. Cek apakah pengguna sudah login
+    - Jika session 'username' tidak ada, arahkan pengguna ke halaman login.
+
+3. Logout pengguna
+    - Kosongkan semua data session dengan session_unset().
+    - Hancurkan session dengan session_destroy().
+    - Tampilkan pesan alert bahwa pengguna berhasil logout.
+    - Arahkan pengguna ke halaman landing_page.php setelah logout.
+
+*/
 session_start();
 if (!isset($_SESSION['username'])) {
 header("Location: login.php");
